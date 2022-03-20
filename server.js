86a98e0504ac6ca6dd293db663563f0f4b2d8461
michaelrios2015 <michael.rios093@gmail.com>
2022-03-20 17:59:18 -0400

    what is happening

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
-// we have started using nodemon npm i nodemon --save-dev
-//  and in the script we us "start:dev": "nodemon server"
-// start:dev because the default is start but start does not need to listen
-// changes to the server and in teh scripts becuase when you run npm run it
-// looks in the scripts in the package json
-
-// so node server.js runs server.js or whatever file you put there
-
-// npm run needs a script in the package json
-
-// nodemon is just a bit more code so that when you make changes the
-// code will be run automatically
-
-//  should work ./node_modules/.bin/nodemon server.js but I get an error
-// message about running scripts... might look into later 
-
-// it does make everthing a lot simplier take away point is that is you go
-// into the node modules folder you can find these programs and execute them
-// but it is easier to put a script in the package json and npm run it
-
-// also now we have a node_modules folder and it get's big fast...
-// it is probably following a specific form or some such 
-
-// but now we need to start using gitingnore or we should start using it 
-
+const path = require('path');
 // of so this is the start of express which is just a bunch of pre written 
 // code to make using the node HTTP thing from day 7 easier to use and
 // it does... lot less code.. much of it just magic to me but that is ok
@@ -40,41 +16,102 @@ const app = express();
 // I guess so
 // console.log(app.toString());
 
+// so this is an example of use and middleware so I am still fuzzy on use... i know get is looking for a specifc path
+// so I guess use is for other things (possibly) everything else and morgan is middleware..is my regular function also
+// middle ware.. maybe anyway morgan shows me what request I sent and how they were answered so nice 
+app.use(require('morgan')('dev'));
 
-// so I think this is the main gain, instead of having a bunch of if statement
+// so I think this is the main gain, instead of having a bunch of if statements
 // we can just tell it the address we are looking for we also get to exclude
 // some lines of code... not sure how we do a catch all and next is imporant
 // but not entirely sure how
 
+// another example of app.use so I guess whenever it sees /assests it know to use this 
+// I know we need to use this... I don't quite understand why..css, pictures, these are all considered static
+app.use('/assests', express.static(path.join(__dirname, 'assests')));
+
+// so this is the first example of a component and we make it a function so we can accept and use varibles
+// it's pretty neat you can start to see the strength of node this does make maintaince much easier 
+const nav = ( url ) => {
+    return `
+    <nav>
+         <a href='/' class='${ url === '/' ?  'selected': '' }'>Home</a>
+         <a href='/rams' class='${ url.startsWith('/rams')  ?  'selected': '' }' >Rams</a>
+    </nav>
+    `;
+}
+
+//  this is fine too but you can't take in variables
+const test = 'test test test';
+
+
 app.get('/', (req, res, next)=> {
     res.send(`
     <html>
-    <body>
-        <h1>!!FORDHAM!!</h1>
-    </body>
+        <head>
+            <link rel='stylesheet' href='/assests/styles.css' />
+        </head>
+        <body>
+             ${ nav(req.url) }
+            <h1>!!FORDHAM!!</h1>
+            ${ test }
+            </body>
+
     </html>`)
 })
 
-
+// are all the (req, res, next)=> functions middle ware
+// the req(request?? comes with a lot of useful information)
 app.get('/rams', (req, res, next)=> {
     res.send(`
     <html>
+        <head>
+            <link rel='stylesheet' href='/assests/styles.css' />
+        </head>
     <body>
+        ${ nav(req.url) }
         <h1>!!rams!!</h1>
+        <ul>
+            <li><a href='/rams/1'>Ramesy</a></li>
+            <li><a href='/rams/5'>RamVan</a></li>
+            <li><a href='/rams/7'>Ram</a></li>
+        <ul>
     </body>
     </html>`)
 })
 
 
+app.get('/rams/:id', (req, res, next)=> {
+    
+    // console.log is going in the terminal... I guess because this is the server?? 
+    // not entirely sure will see when it just shows up in the website 
+     console.log(req)
+    //  console.log('HI');
 
+    res.send(
+        `
+    <html>
+        <head>
+            <link rel='stylesheet' href='/assests/styles.css' />
+        </head>
+    <body>
+        ${ nav(req.url) }
+        <h1><a href='/rams'>!!rams!!</a></h1>
+        To do: information about a user with an if of ${req.params.id}
+    </body>
+    </html>`)
+})
 
 
 // this is the same as without express process.env.PORT is just what we us when deployed is it always this, IDK
+// apperently on a Mac you can just type in PORT = 3001 in the terminal and it will know that is the process.env 
+// interesting 
 const port = process.env.PORT || 3000;
 
 // I think this is a bit less but even if it is not now it tells you 
 // why it fails
 app.listen(port, ()=> console.log(`listening on port ${port}`));
+
 
 
 
